467402b97b468db8f892b565390eb61ff6820ac4
himanshuGaur684 <60474413+himanshuGaur684@users.noreply.github.com>
2021-05-07 13:16:20 +0530

    Custom Data Binding Adapters

--- app/src/main/java/gaur/himanshu/august/databindingplayground/MainActivity.kt
+++ app/src/main/java/gaur/himanshu/august/databindingplayground/MainActivity.kt
@@ -23,5 +23,11 @@ class MainActivity : AppCompatActivity() {
         binding.url =
             "http://1.bp.blogspot.com/-B0WT_f1kqL8/Ub0pQUt9h7I/AAAAAAAAAn8/byzZbyzOyQ8/s1600/nexussamsungkindlefire.jpg"
 
+
+        // image url using in this video are
+        // http://3.bp.blogspot.com/-7-KmADvPxkY/UgitVrQvgiI/AAAAAAAAASo/lgZiaL6RfnY/s1600/Android+OS+for+Smartphone+Download+Free.jpg
+        // https://bgr.com/wp-content/uploads/2021/05/google-entertainment-space-android-tablets.jpg?resize=300  ( for showing the placrholder images )
+        // http://1.bp.blogspot.com/-B0WT_f1kqL8/Ub0pQUt9h7I/AAAAAAAAAn8/byzZbyzOyQ8/s1600/nexussamsungkindlefire.jpg ( for showing the error image )
+
     }
 }
